c160bb3074afd64a678d413a71584e902c08bf34
BaptistePR <klevh1@gmail.com>
2019-03-13 18:01:07 +0100

    updating version

--- SANDAL2.h
+++ SANDAL2.h
@@ -10,7 +10,7 @@ extern "C" {
 /**
  * @file SANDAL2.h
  * @author Baptiste PRUNIER (KLEVH)
- * @version 1.3.0
+ * @version 1.4.0
  * @brief SANDAL2.h is the file to include which includes everything else.
  *  Compilation flags to use :
  *    -lSDL2 -lSDL2_ttf -lSDL2_image -lm
