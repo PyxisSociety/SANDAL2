6d45e5678a9d4a326af32004a8c5345badb40e2f
BaptistePR <klevh1@gmail.com>
2019-03-20 14:58:44 +0100

    updating run_all script

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -2,16 +2,11 @@
 
 dirs=`ls --hide=LAssert`
 
-fail=0
 for d in ${dirs}; do
     if [ -d "${d}" ]; then
-        cd $1
+        cd ${d}
         make
         ../run.sh
-        fail=$?
         cd ..
-    fi
-    if [ "${fail}" != "0" ]; then
-        break
     fi
 done
